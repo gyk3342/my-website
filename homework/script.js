008eaea15a8129ffea2154d07a984c78aa4a48f1
gyk3342 <149049495+gyk3342@users.noreply.github.com>
2024-07-22 13:42:19 +0900

    Create script.js

---
+++ homework/script.js
@@ -0,0 +1,14 @@
+function submitQuiz() {
+    const form = document.getElementById('quizForm');
+    const playerName = form.playerName.value;
+    let score = 0;
+    const correctAnswers = [1, 1, 1, 2, 2, 1, 1, 1, 3, 2]; // Indices of correct answers (starting from 0)
+
+    correctAnswers.forEach((correctAnswer, index) => {
+        const question = `q${index + 1}`;
+        const selectedAnswer = form[question].value;
+        score += parseInt(selectedAnswer, 10);
+    });
+
+    alert(`Hello ${playerName}, your score is ${score * 10}`);
+}
